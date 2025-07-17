c3b1f22f72cfd6fcbbbd1aeee13d08d579fa0c36
FilipMz <149836871+FilipMz@users.noreply.github.com>
2025-07-18 00:33:29 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Filip Mzyk personal website';
+export const SITE_DESCRIPTION = 'This is my personal website showcasing my CV, some projects, and a few blog posts';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
