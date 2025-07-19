32782a355caf7931cc2af75148428d8d10164bf5
FilipMz <149836871+FilipMz@users.noreply.github.com>
2025-07-19 23:42:52 +0200

    Create projects.ts

---
+++ src/data/projects.ts
@@ -0,0 +1,24 @@
+export interface Project {
+  title: string;
+  img: string;
+  desc: string;
+  url: string;
+  badge?: string;
+}
+
+export const projects: Project[] = [
+  {
+    title: "Test1",
+    img: "public/itemPreview.webp",
+    desc: "Placeholder.",
+    url: "!#",
+    badge: "NEW",
+  },
+  {
+    title: "Test2",
+    img: "public/itemPreview.webp",
+    desc: "Placeholder.",
+    url: "!#",
+  },
+  /* add more here */
+];
